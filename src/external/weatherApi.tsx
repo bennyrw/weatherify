2deed56507f7126736c0a2c953f4243efdd05644
Ben Morris <ben.morris@camcog.com>
2020-05-01 23:30:46 +0100

    Resolve a couple of todos

--- src/external/weatherApi.tsx
+++ src/external/weatherApi.tsx
@@ -1,14 +1,21 @@
 import { WeatherData } from '../types';
 import { List } from 'immutable';
 
+const MAX_DAYS = 5;
+
+/**
+ * Get the upcoming daily weather for a specific location.
+ * @param longitude
+ * @param latitude 
+ * @return Promise yielding up to 5 days of forecasted weather.
+ */
 export async function getLocationDailyWeather(longitude: number, latitude: number): Promise<List<WeatherData>> {
     try {
         const response = await fetch(getRequestUrl(longitude, latitude));
         const json = await response.json();
         const dailyData = json.daily as Array<ApiDailyData>
 
-        // todo - we only want max 5 according to spec
-        return List(dailyData.map(parseDailyData));
+        return List(dailyData.slice(0, MAX_DAYS).map(parseDailyData));
     } catch (e) {
         throw new Error(`Failed to get weather for location, error: ${e.message}`);
     }
@@ -50,7 +57,6 @@ const getIconUrl = (icon: string) =>
 // of your own, have that make the API call instead of the client JS. Additional options here:
 // https://developers.google.com/maps/api-key-best-practices
 const API_KEY = 'e56d308d4e223c8749d63fe32d8b8442';
-// todo - is it obfuscated in built code?
 
 const getRequestUrl = (longitude: number, latitude: number) =>
     `https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&units=metric&appid=${API_KEY}`;
--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
-//import * as serviceWorker from './serviceWorker';
 import { Provider } from 'react-redux';
-import { reducer } from './reducers';
 import { createStore, applyMiddleware } from 'redux';
 import createSagaMiddleware from 'redux-saga';
+
+import App from './App';
+import { reducer } from './reducers';
 import { StoreState, getInitialState } from './store';
 import { Action } from './actions';
 import { fetchForecastSaga } from './sagas';
@@ -26,9 +26,3 @@ ReactDOM.render(
 );
 
 sagaMiddleware.run(fetchForecastSaga);
-
-// todo - remove
-// // If you want your app to work offline and load faster, you can change
-// // unregister() to register() below. Note this comes with some pitfalls.
-// // Learn more about service workers: https://bit.ly/CRA-PWA
-// serviceWorker.unregister();
