df177244ac095acf4d48ae5a3caa02c39e23bbf7
Ben Morris <ben.morris@camcog.com>
2020-05-02 22:04:42 +0100

    Deploy build from caf7fd3

---
+++ precache-manifest.d64673c077adb4c7488a706baf53782c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ecb6a9fa5d57d7a8e7d7ea2eb11f0059",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d2439f19b1b9edab4591",
+    "url": "/static/js/2.d92cb87a.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.d92cb87a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2e37d962afcb27fe75c4",
+    "url": "/static/js/main.efb604f7.chunk.js"
+  },
+  {
+    "revision": "cb889718d29d10828357",
+    "url": "/static/js/runtime-main.a92b00be.js"
+  }
+]);
