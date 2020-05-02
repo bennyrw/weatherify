6bc332cbc13678e9cf452923d15687a0b7d484bb
Ben Morris <ben.morris@camcog.com>
2020-05-02 22:14:34 +0100

    Use relative paths for github pages

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.d64673c077adb4c7488a706baf53782c.js"
+  "precache-manifest.d64673c077adb4c7488a706baf53782c.js"
 );
 
 self.addEventListener('message', (event) => {
